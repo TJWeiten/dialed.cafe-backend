a3aa3cbc731f630d023e376f28bcd8b75a627a49
T.J. Weiten <me@tjweiten.com>
2025-09-29 23:04:33 -0700

    Bootstrap

---
+++ src/main/java/cafe/dialed/DialedApplication.java
@@ -0,0 +1,13 @@
+package cafe.dialed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialedApplication.class, args);
+	}
+
+}
