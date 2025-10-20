58fa5eeda59de5a6d9ebbd7bfdbb5d8051f6ba0a
T.J. Weiten <me@tjweiten.com>
2025-10-19 20:55:24 -0700

    Fix a silly mistake

--- src/main/java/cafe/dialed/controllers/HealthCheckController.java
+++ src/main/java/cafe/dialed/controllers/HealthCheckController.java
@@ -1,7 +1,7 @@
 package cafe.dialed.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +10,8 @@ public class HealthCheckController {
     public HealthCheckController() {
     }
 
-    @PostMapping("/healthcheck")
-    public ResponseEntity<String> handleClerkWebhook() {
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("I'm doing great!");
     }
 
